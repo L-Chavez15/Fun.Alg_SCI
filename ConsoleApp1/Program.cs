777e793056f76b4d5f3c16d6b61a68fd4eeeed35
n00449821@upn.pe <n00449821pn.pe>
2025-05-21 13:38:57 -0500

    mostramos mensajes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -10,7 +11,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            Pisos();
         }
         static void Pisos()
         {
@@ -93,11 +94,13 @@ namespace ConsoleApp1
             Console.ResetColor();
             float tem;
             Random R = new Random();
-            tem = R.Next(20, 150);
+            tem = R.Next(20, 500);
             float humo;
             Random R1 = new Random();
             humo = R1.Next(0, 51);
-            Console.WriteLine("HOLA Piso 1");
+            Datos(tem);
+            Evacuación(tem,humo);
+
         }
         static void Piso2()
         {
@@ -109,27 +112,106 @@ namespace ConsoleApp1
             Console.ResetColor();
             float tem;
             Random R = new Random();
-            tem = R.Next(20, 150);
+            tem = R.Next(20, 500);
             float humo;
             Random R1 = new Random();
             humo = R1.Next(0, 51);
-            Console.WriteLine("HOLA Piso 2");
+            Datos(tem);
+            Evacuación(tem, humo);
         }
         static void Piso3()
         {
             Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("=========================================");
-            Console.WriteLine("\t\tPISO 3");
-            Console.WriteLine("=========================================\n");
-            Console.ResetColor();
             float tem;
             Random R = new Random();
-            tem = R.Next(20, 150);
+            tem = R.Next(20, 500);
             float humo;
             Random R1 = new Random();
             humo = R1.Next(0, 51);
-            Console.WriteLine("HOLA Piso 3");
+            Datos(tem);
+            Evacuación(tem, humo);
+        }
+        static void Datos (float temp)
+        {
+            Console.WriteLine("Temperatura: ");
+            if (temp > 56)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (temp > 35)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            Console.WriteLine(temp + "°C");
+            Console.ResetColor ();
+
+            
+            Console.WriteLine("SENSORES: ");
+            
+            if (temp > 50)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("ENCENDIDOS\n");
+                Console.ResetColor();
+            }
+            
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("APAGADOS\n");
+                Console.ResetColor();
+            }
+            Console.WriteLine("LUCES ESTROBOSCÓPICAS: ");
+
+            if (temp > 50)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("ENCENDIDOS\n");
+                Console.ResetColor();
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("APAGADOS\n");
+                Console.ResetColor();
+            }
+
+
+        }
+        static void Evacuación(float temp, float sensores)
+        {
+            Console.WriteLine("\nRecomendación:");
+            if (temp > 80 || sensores > 70)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("¡EVACUAR INMEDIATAMENTE!");
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.Beep(1000, 300); 
+                    Thread.Sleep(200);       
+                }
+            }
+            else if (temp > 70 || sensores > 50)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("¡Posible incendio! Verificar área");
+            }
+            else if (temp > 50 || sensores > 30)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Condiciones anormales - Monitorear");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Todo normal");
+            }
+            Console.ResetColor();
         }
 
     }
